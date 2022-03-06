94bb2336cb038fc3a027eb596646f801635c8f19
joshua-8 <joshua_phelps@brown.edu>
2022-03-06 16:57:45 -0500

    simple_blinking working

--- commTest/commTest-esp32/src/main.cpp
+++ commTest/commTest-esp32/src/main.cpp
@@ -1,9 +1,16 @@
 #include <Arduino.h>
+
 void setup()
 {
     pinMode(2, OUTPUT);
+    Serial2.begin(9600, SERIAL_8N1, 16, 17);
 }
 void loop()
 {
-    digitalWrite(2, (millis() % 1100) < 900);
+    delay(1000);
+    digitalWrite(2, HIGH);
+    Serial2.print("n");
+    delay(1000);
+    digitalWrite(2, LOW);
+    Serial2.print("f");
 }
--- commTest/commTest-nano/src/main.cpp
+++ commTest/commTest-nano/src/main.cpp
@@ -1,9 +1,22 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(2, 3);
 void setup()
 {
     pinMode(13, OUTPUT);
+    mySerial.begin(9600);
+    Serial.begin(115200);
 }
 void loop()
 {
-    digitalWrite(13, (millis() % 1000) < 100);
+    if (mySerial.available() > 0) {
+        char inChar = mySerial.read();
+        if (inChar == 'f') {
+            digitalWrite(13, LOW);
+        }
+        if (inChar == 'n') {
+            digitalWrite(13, HIGH);
+        }
+    }
+    delay(100);
 }
