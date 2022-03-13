5b5d52d5dd204f0e25d19bf00a3fe9b15530ed74
joshua-8 <joshua_phelps@brown.edu>
2022-03-13 17:59:56 -0400

    display working, simple communication

--- commTest/commTest-nano/src/main.cpp
+++ commTest/commTest-nano/src/main.cpp
@@ -1,20 +1,39 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_LEDBackpack.h>
 #include <Arduino.h>
+#include <SPI.h>
 #include <SoftwareSerial.h>
+
 SoftwareSerial mySerial(2, 3);
+Adafruit_7segment display = Adafruit_7segment();
+
 void setup()
 {
     pinMode(13, OUTPUT);
     mySerial.begin(9600);
     Serial.begin(115200);
+    display.begin(0x70);
 }
+
 void loop()
 {
+
     if (mySerial.available() > 0) {
         char inChar = mySerial.read();
         if (inChar == 'f') {
+            display.clear();
+            display.writeDisplay();
+            display.println("your");
+            display.writeDisplay();
+
             digitalWrite(13, LOW);
         }
         if (inChar == 'n') {
+            display.clear();
+            display.writeDisplay();
+            display.println("sus");
+            display.writeDisplay();
+
             digitalWrite(13, HIGH);
         }
     }
